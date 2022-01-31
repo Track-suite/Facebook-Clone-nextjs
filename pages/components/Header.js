ac291308895271c1912793ccbefc3c782f6e47da
Asram Ahamed <asrama1998@gmail.com>
2022-01-31 16:17:49 +0530

    fixed some error in tailwindcss

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Image from "next/image";
 import {
   BellIcon,
@@ -18,19 +19,21 @@ import {
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1 className="">Header</h1>
+      <div className="flex items-center">
+        {/* Left */}
 
-      {/* Left */}
-
-      <Image
-        alt="facebook"
-        src="https://links.papareact.com/5me"
-        width={40}
-        height={40}
-        layout="fixed"
-      />
-      <div>
-        <input type="text" placeholder="Search Facebook" />
+        <Image
+          alt="facebook"
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 item-center">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
 
       {/* Center */}
