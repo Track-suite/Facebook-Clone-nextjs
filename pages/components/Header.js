2ffab8d33c757cb108ed97b5b0955802637fdcd6
Asram Ahamed <asrama1998@gmail.com>
2022-01-31 15:11:53 +0530

    added fb icon

---
+++ pages/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+
+      {/* Left */}
+
+      <Image
+        alt="facebook"
+        src="https://links.papareact.com/5me"
+        width={40}
+        height={40}
+        layout="fixed"
+      />
+      <div>
+        <input type="text" placeholder="Search Facebook" />
+      </div>
+
+      {/* Center */}
+
+      {/* Right */}
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
