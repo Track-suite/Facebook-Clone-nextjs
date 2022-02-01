884e00cf7902363dbdc8e92a76fbd52ef672968a
Asram Ahamed <asrama1998@gmail.com>
2022-02-01 14:07:51 +0530

    created a header and the next auth

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -16,9 +16,15 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
+import HeaderIcon from "./HeaderIcon";
+
 const Header = () => {
   return (
-    <div>
+    <div
+      className="sticky top-0 z-50 bg-white 
+    flex items-center p2
+     lg:px-5 shadow-md"
+    >
       <div className="flex items-center">
         {/* Left */}
 
@@ -30,9 +36,10 @@ const Header = () => {
           layout="fixed"
         />
         <div className="flex ml-2 item-center rounded-full bg-gray-100 p-2">
-          <SearchIcon className="h-6" />
+          <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-502"
+            className=" hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none 
+            placeholder-gray-502 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
@@ -40,8 +47,25 @@ const Header = () => {
       </div>
 
       {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2  ">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile pic */}
+        <p className="whitespace-nowrap font-semibold pr-3">Asram Ahamed</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 };
